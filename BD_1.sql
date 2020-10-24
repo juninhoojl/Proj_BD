7a0e45e1e88d9b5a080bce6c319a97bca7ad9ed5
juninhoojl <juninhopc@icloud.com>
2020-10-24 19:18:59 -0300

    Desajustado

--- BD_1.sql
+++ BD_1.sql
@@ -20,12 +20,14 @@ CREATE TABLE Vacina(
 
 ALTER SEQUENCE Vacina_id_seq OWNED BY Vacina.ID;
 
+CREATE TYPE grupos AS ENUM ('Criancas', 'Adultos', 'Idosos', 'Profissionais de Saude', 'Grupo de Risco', 'Outro');
+
 CREATE SEQUENCE Distribuicao_id_seq;
 
 CREATE TABLE Distribuicao(
 	ID INT NOT NULL DEFAULT nextval('Distribuicao_id_seq') PRIMARY KEY,
 	Quantidade INTEGER NOT NULL,
-	Grupo VARCHAR(25) NOT NULL,
+	Grupo grupos NOT NULL,
 	Data TIMESTAMP NOT NULL,
 	Local VARCHAR(25) NOT NULL,
 	Vacina INT NOT NULL,
@@ -33,17 +35,48 @@ CREATE TABLE Distribuicao(
 		ON DELETE CASCADE ON UPDATE CASCADE
 );
 
+CREATE SEQUENCE Teste_id_seq;
+CREATE TABLE Teste(
+	ID INT NOT NULL DEFAULT nextval('Teste_id_seq') PRIMARY KEY,
+	Vacina INT NOT NULL,
+	Quantidade INTEGER NOT NULL,
+	DataInicio TIMESTAMP NOT NULL,
+	DataFim TIMESTAMP NOT NULL,
+	Local VARCHAR(25) NOT NULL
+);
+ALTER SEQUENCE Teste_id_seq OWNED BY Teste.ID;
+
+
 CREATE SEQUENCE Acompanhamento_id_seq;
 
 CREATE TABLE Acompanhamento(
 	ID INT NOT NULL DEFAULT nextval('Acompanhamento_id_seq') PRIMARY KEY,
 	Quantidade INTEGER NOT NULL,
 	Status VARCHAR(25) NOT NULL,
-	Biotipo VARCHAR(25) NOT NULL,
-	Local VARCHAR(25) NOT NULL
+	Biotipo Biotipo grupos NOT NULL,
+	Local VARCHAR(25) NOT NULL,
+	PassaraPor INT NOT NULL,
+	FOREIGN KEY (PassaraPor) REFERENCES PassaraPor(ID)
+		ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 ALTER SEQUENCE Acompanhamento_id_seq OWNED BY Acompanhamento.ID;
+
+
+CREATE SEQUENCE PassaraPor_id_seq;
+CREATE TABLE PassaraPor(
+	ID INT NOT NULL DEFAULT nextval('PassaraPor_id_seq') PRIMARY KEY,
+	Vacina INT NOT NULL,
+	Teste INT NOT NULL,
+	FOREIGN KEY (Vacina) REFERENCES Vacina(ID)
+		ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY (Teste) REFERENCES Teste(ID)
+		ON DELETE CASCADE ON UPDATE CASCADE
+);
+ALTER SEQUENCE PassaraPor_id_seq OWNED BY PassaraPor.ID;
+
+
+
 
 CREATE SEQUENCE Pais_id_seq;
 
@@ -53,40 +86,13 @@ CREATE TABLE Pais (
 	ID INT NOT NULL DEFAULT nextval('Pais_id_seq') PRIMARY KEY,
 	Nome VARCHAR(25) NOT NULL,
 	Sigla VARCHAR(3) NOT NULL,
-	Continente continentes  NOT NULL
+	Continente continentes NOT NULL
 );
 
 ALTER SEQUENCE Pais_id_seq OWNED BY Pais.ID;
 
-CREATE SEQUENCE PassaraPor_id_seq;
 
-CREATE TABLE PassaraPor(
-	ID INT NOT NULL DEFAULT nextval('PassaraPor_id_seq') PRIMARY KEY,
-	Vacina INT NOT NULL,
-	Acompanhamento INT NOT NULL,
-	FOREIGN KEY (Vacina) REFERENCES Vacina(ID)
-		ON DELETE CASCADE ON UPDATE CASCADE,
-	FOREIGN KEY (Acompanhamento) REFERENCES Acompanhamento(ID)
-		ON DELETE CASCADE ON UPDATE CASCADE,
-	PRIMARY KEY(ID, Vacina, Acompanhamento)
-);
 
-ALTER SEQUENCE PassaraPor_id_seq OWNED BY PassaraPor.ID;
-
-CREATE SEQUENCE Teste_id_seq;
-
-CREATE TABLE Teste(
-	ID INT NOT NULL DEFAULT nextval('Teste_id_seq') PRIMARY KEY,
-	Quantidade INTEGER NOT NULL,
-	DataInicio TIMESTAMP NOT NULL,
-	DataFim TIMESTAMP NOT NULL,
-	Local VARCHAR(25) NOT NULL,
-	PassaraPor INT,
-	FOREIGN KEY (PassaraPor) REFERENCES PassaraPor(ID)
-		ON DELETE CASCADE ON UPDATE CASCADE
-);
-
-ALTER SEQUENCE Teste_id_seq OWNED BY Teste.ID;
 
 CREATE SEQUENCE Instituicao_id_seq;
 
@@ -210,6 +216,60 @@ INSERT INTO RelInstVac (ID, Vacina, Instituicao)
 INSERT INTO RelInstVac (ID, Vacina, Instituicao)
 	VALUES (DEFAULT, 5, 5);
 
+
+-- Distribuicao
+
+INSERT INTO Distribuicao (ID, Quantidade, Grupo, Data, Local, Vacina)
+	VALUES (DEFAULT, 1000, 'Criancas', current_timestamp, 'Itajuba', 1);
+
+INSERT INTO Distribuicao (ID, Quantidade, Grupo, Data, Local, Vacina)
+	VALUES (DEFAULT, 1500, 'Idosos', current_timestamp, 'Piranguinho', 1);
+
+INSERT INTO Distribuicao (ID, Quantidade, Grupo, Data, Local, Vacina)
+	VALUES (DEFAULT, 2000, 'Adultos', current_timestamp, 'Campos do Jordao', 3);
+
+INSERT INTO Distribuicao (ID, Quantidade, Grupo, Data, Local, Vacina)
+	VALUES (DEFAULT, 900, 'Profissionais de Saude', current_timestamp, 'Cristina', 3);
+
+INSERT INTO Distribuicao (ID, Quantidade, Grupo, Data, Local, Vacina)
+	VALUES (DEFAULT, 1300, 'Grupo de Risco', current_timestamp, 'Itajuba', 3);
+
+
+-- Teste
+
+INSERT INTO Teste (ID, Quantidade, DataInicio, DataInicio, Local)
+	VALUES (DEFAULT, 1300, current_timestamp, current_timestamp, 'Itajuba');
+
+INSERT INTO Teste (ID, Quantidade, DataInicio, DataInicio, Local)
+	VALUES (DEFAULT, 5000, current_timestamp, current_timestamp, 'China');
+
+INSERT INTO Teste (ID, Quantidade, DataInicio, DataInicio, Local)
+	VALUES (DEFAULT, 10000, current_timestamp, current_timestamp, 'Estado de Sao Paulo');
+
+INSERT INTO Teste (ID, Quantidade, DataInicio, DataInicio, Local)
+	VALUES (DEFAULT, 7000, current_timestamp, current_timestamp, 'Estado do Rio de Janeiro');
+
+INSERT INTO Teste (ID, Quantidade, DataInicio, DataInicio, Local)
+	VALUES (DEFAULT, 2000, current_timestamp, current_timestamp, 'Ile de France');
+
+
+-- Acompanhamento
+
+INSERT INTO Acompanhamento (ID, Quantidade, Status, Biotipo, Local)
+	VALUES (DEFAULT, 5000, "Blah Blah", current_timestamp, 'China');
+
+	CREATE SEQUENCE Acompanhamento_id_seq;
+
+	CREATE TABLE Acompanhamento(
+		ID INT NOT NULL DEFAULT nextval('Acompanhamento_id_seq') PRIMARY KEY,
+		Quantidade INTEGER NOT NULL,
+		Status VARCHAR(25) NOT NULL,
+		Biotipo grupos NOT NULL,
+		Local VARCHAR(25) NOT NULL
+	);
+
+
+	CREATE TYPE grupos AS ENUM ('Criancas', 'Adultos', 'Idosos', 'Profissionais de Saude', 'Grupo de Risco', 'Outro');
 
 
 -- pg_dump -U joseluizjunior -W -F p projeto_bd1 > ~/Documents/Backup_1
