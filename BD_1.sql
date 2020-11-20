b46b57ebf45fb23a99a85a169d4baa04d201a3ce
juninhoojl <juninhopc@icloud.com>
2020-11-19 23:37:11 -0300

    Organizacao

--- BD_1.sql
+++ BD_1.sql
@@ -1,9 +1,8 @@
 -- DROP DATABASE IF EXISTS projeto_bd1;
 -- CREATE DATABASE projeto_bd1; 
 -- USE projeto_bd1;
-
-DROP SCHEMA public CASCADE;
-CREATE SCHEMA public;
+-- DROP SCHEMA public CASCADE;
+-- CREATE SCHEMA public;
 
 GRANT ALL ON SCHEMA public TO postgres;
 GRANT ALL ON SCHEMA public TO public;
@@ -49,22 +48,29 @@ CREATE TABLE Pais (
 );
 ALTER SEQUENCE Pais_id_seq OWNED BY Pais.ID;
 
-
 CREATE SEQUENCE Teste_id_seq;
 CREATE TABLE Teste(
 	ID INT NOT NULL DEFAULT nextval('Teste_id_seq') PRIMARY KEY,
 	Vacina INT NOT NULL,
-	Pais INT NOT NULL,
 	Quantidade INTEGER NOT NULL,
 	DataInicio TIMESTAMP NOT NULL,
 	DataFim TIMESTAMP NOT NULL,
 	Local VARCHAR(25) NOT NULL,
 	FOREIGN KEY (Vacina) REFERENCES Vacina(ID)
+		ON DELETE CASCADE ON UPDATE CASCADE
+);
+ALTER SEQUENCE Teste_id_seq OWNED BY Teste.ID;
+
+
+CREATE TABLE RelTestPai(
+	Teste INT NOT NULL,
+	Pais INT NOT NULL,
+	PRIMARY KEY(Teste, Pais),
+	FOREIGN KEY (Teste) REFERENCES Teste(ID)
 		ON DELETE CASCADE ON UPDATE CASCADE,
 	FOREIGN KEY (Pais) REFERENCES Pais(ID)
 		ON DELETE CASCADE ON UPDATE CASCADE
 );
-ALTER SEQUENCE Teste_id_seq OWNED BY Teste.ID;
 
 CREATE TYPE tipostatus AS ENUM ('Ocorrendo', 'Finalizado', 'Abortado', 'Iniciado');
 CREATE SEQUENCE Acompanhamento_id_seq;
@@ -73,7 +79,6 @@ CREATE TABLE Acompanhamento(
 	Teste INT NOT NULL,
 	Quantidade INTEGER NOT NULL,
 	Status tipostatus NOT NULL,
-	Observacoes VARCHAR(50),
 	Biotipo grupos NOT NULL,
 	Local VARCHAR(25) NOT NULL,
 	FOREIGN KEY (Teste) REFERENCES Teste(ID)
@@ -81,34 +86,43 @@ CREATE TABLE Acompanhamento(
 );
 ALTER SEQUENCE Acompanhamento_id_seq OWNED BY Acompanhamento.ID;
 
--- ATE AQUI OK
-
 CREATE SEQUENCE Instituicao_id_seq;
-CREATE TYPE tipoinsti AS ENUM ('Publica', 'Privada');
 CREATE TABLE Instituicao(
 	ID INT NOT NULL DEFAULT nextval('Instituicao_id_seq') PRIMARY KEY,
-	Tipo tipoinsti NOT NULL,
 	Pais INT NOT NULL,
 	Nome VARCHAR(25) NOT NULL,
 	Local VARCHAR(25) NOT NULL,
-	GovOuBenef VARCHAR(25) NOT NULL,
+	Governo VARCHAR(25),
+	Beneficiario VARCHAR(25),
 	FOREIGN KEY (Pais) REFERENCES Pais(ID)
 		ON DELETE CASCADE ON UPDATE CASCADE,
 	UNIQUE(Nome)
 );
 ALTER SEQUENCE Instituicao_id_seq OWNED BY Instituicao.ID;
 
-CREATE SEQUENCE RelInstVac_id_seq;
+
 CREATE TABLE RelInstVac(
-	ID INT NOT NULL DEFAULT nextval('RelInstVac_id_seq') PRIMARY KEY,
 	Vacina INT NOT NULL,
 	Instituicao INT NOT NULL,
+	PRIMARY KEY(Vacina, Instituicao),
 	FOREIGN KEY (Vacina) REFERENCES Vacina(ID)
 		ON DELETE CASCADE ON UPDATE CASCADE,
 	FOREIGN KEY (Instituicao) REFERENCES Instituicao(ID)
 		ON DELETE CASCADE ON UPDATE CASCADE
 );
-ALTER SEQUENCE RelInstVac_id_seq OWNED BY RelInstVac.ID;
+
+CREATE TABLE RelVacTestAcom(
+	Vacina INT NOT NULL,
+	Teste INT NOT NULL,
+	Acompanhamento INT NOT NULL,
+	PRIMARY KEY(Vacina, Teste, Acompanhamento),
+	FOREIGN KEY (Vacina) REFERENCES Vacina(ID)
+		ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY (Teste) REFERENCES Teste(ID)
+		ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY (Acompanhamento) REFERENCES Acompanhamento(ID)
+		ON DELETE CASCADE ON UPDATE CASCADE
+);
 
 -- Pais
 INSERT INTO Pais (ID, Nome, Sigla, Continente)
@@ -129,65 +143,62 @@ INSERT INTO Pais (ID, Nome, Sigla, Continente)
 INSERT INTO Pais (ID, Nome, Sigla, Continente)
 	VALUES (DEFAULT, 'Japao', 'JP', 'Asia');
 
-
 -- Vacina
 
 INSERT INTO Vacina (ID, Nome, Fase, Principio)
 	VALUES (DEFAULT, 'Sinovac', '3', 'Inativada');
 
 INSERT INTO Vacina (ID, Nome, Fase, Principio)
-	VALUES (DEFAULT, 'Coronavac', '3', 'Inativada');
+	VALUES (DEFAULT, 'Coronavac', '1', 'Inativada');
 
 INSERT INTO Vacina (ID, Nome, Fase, Principio)
-	VALUES (DEFAULT, 'AstraZeneca', '3', 'Toxoide');
+	VALUES (DEFAULT, 'AstraZeneca', '2', 'Toxoide');
 
 INSERT INTO Vacina (ID, Nome, Fase, Principio)
-	VALUES (DEFAULT, 'Pfizer', '3', 'Imunoglobulina');
+	VALUES (DEFAULT, 'Pfizer', '1', 'Imunoglobulina');
 
 INSERT INTO Vacina (ID, Nome, Fase, Principio)
 	VALUES(DEFAULT, 'Moderna', '3', 'Atenuada');
 
-
 -- Instituicao
 
-INSERT INTO Instituicao (ID, Tipo, Pais, Nome, Local, GovOuBenef)
-	VALUES (DEFAULT, 'Publica', 1, 'UNIFEI', 'Itajuba', 'Federal');
+INSERT INTO Instituicao (ID, Pais, Nome, Local, Governo, Beneficiario)
+	VALUES (DEFAULT, 1, 'UNIFEI', 'Itajuba', 'Federal', '');
 
-INSERT INTO Instituicao (ID, Tipo, Pais, Nome, Local, GovOuBenef)
-	VALUES (DEFAULT, 'Privada', 4, 'Samsung', 'Beijing', 'Samsung Inc.');
+INSERT INTO Instituicao (ID, Pais, Nome, Local, Governo, Beneficiario)
+	VALUES (DEFAULT, 4, 'Samsung', 'Beijing', '', 'Samsung Inc.');
 
-INSERT INTO Instituicao (ID, Tipo, Pais, Nome, Local, GovOuBenef)
-	VALUES (DEFAULT, 'Publica', 3, 'Oxford', 'Oxford', 'Federal');
+INSERT INTO Instituicao (ID, Pais, Nome, Local, Governo, Beneficiario)
+	VALUES (DEFAULT, 3, 'Oxford', 'Oxford', 'Federal', '');
 
-INSERT INTO Instituicao (ID, Tipo, Pais, Nome, Local, GovOuBenef)
-	VALUES (DEFAULT, 'Publica', 1, 'Barraca Amarela', 'Piranguinho', 'Municipal');
+INSERT INTO Instituicao (ID, Pais, Nome, Local, Governo, Beneficiario)
+	VALUES (DEFAULT, 1, 'Barraca Amarela', 'Piranguinho', '', 'Doces 123');
 
-INSERT INTO Instituicao (ID, Tipo, Pais, Nome, Local, GovOuBenef)
-	VALUES (DEFAULT, 'Privada', 5, 'Juji TV', 'Minato Tokyo', 'Fuji Media Holdings, Inc.');
-
+INSERT INTO Instituicao (ID, Pais, Nome, Local, Governo, Beneficiario)
+	VALUES (DEFAULT, 5, 'Juji TV', 'Minato Tokyo', '', 'Fuji Media Holdings, Inc.');
 
 -- RelInstVac
 
-INSERT INTO RelInstVac (ID, Vacina, Instituicao)
-	VALUES (DEFAULT, 1, 1);
+INSERT INTO RelInstVac (Vacina, Instituicao)
+	VALUES (1, 1);
 
-INSERT INTO RelInstVac (ID, Vacina, Instituicao)
-	VALUES (DEFAULT, 1, 2);
+INSERT INTO RelInstVac (Vacina, Instituicao)
+	VALUES (1, 2);
 
-INSERT INTO RelInstVac (ID, Vacina, Instituicao)
-	VALUES (DEFAULT, 2, 2);
+INSERT INTO RelInstVac (Vacina, Instituicao)
+	VALUES (2, 2);
 
-INSERT INTO RelInstVac (ID, Vacina, Instituicao)
-	VALUES (DEFAULT, 3, 3);
+INSERT INTO RelInstVac (Vacina, Instituicao)
+	VALUES (3, 3);
 
-INSERT INTO RelInstVac (ID, Vacina, Instituicao)
-	VALUES (DEFAULT, 4, 4);
+INSERT INTO RelInstVac (Vacina, Instituicao)
+	VALUES (4, 4);
 
-INSERT INTO RelInstVac (ID, Vacina, Instituicao)
-	VALUES (DEFAULT, 5, 4);
+INSERT INTO RelInstVac (Vacina, Instituicao)
+	VALUES (5, 4);
 
-INSERT INTO RelInstVac (ID, Vacina, Instituicao)
-	VALUES (DEFAULT, 5, 5);
+INSERT INTO RelInstVac (Vacina, Instituicao)
+	VALUES (5, 5);
 
 
 -- Distribuicao
@@ -208,44 +219,220 @@ INSERT INTO Distribuicao (ID, Quantidade, Grupo, Data, Local, Vacina)
 	VALUES (DEFAULT, 1300, 'Grupo de Risco', current_timestamp, 'Itajuba', 3);
 
 
-
--- REFAZER A PARTIR DAQUI
-
-
 -- Teste
 
-INSERT INTO Teste (ID, Vacina, Pais, Quantidade, DataInicio, DataFim, Local)
-	VALUES (DEFAULT, 1, 1, 1300, current_timestamp, current_timestamp, 'Itajuba');
+INSERT INTO Teste (ID, Vacina, Quantidade, DataInicio, DataFim, Local)
+	VALUES (DEFAULT, 1, 1300, current_timestamp, current_timestamp, 'Itajuba');
 
-INSERT INTO Teste (ID, Vacina, Pais, Quantidade, DataInicio, DataFim, Local)
-	VALUES (DEFAULT, 2, 2, 5000, current_timestamp, current_timestamp, 'China');
+INSERT INTO Teste (ID, Vacina, Quantidade, DataInicio, DataFim, Local)
+	VALUES (DEFAULT, 2, 5000, current_timestamp, current_timestamp, 'China');
 
-INSERT INTO Teste (ID, Vacina, Pais, Quantidade, DataInicio, DataFim, Local)
-	VALUES (DEFAULT, 2, 2, 10000, current_timestamp, current_timestamp, 'Estado de Sao Paulo');
+INSERT INTO Teste (ID, Vacina, Quantidade, DataInicio, DataFim, Local)
+	VALUES (DEFAULT, 2, 10000, current_timestamp, current_timestamp, 'Estado de Sao Paulo');
 
-INSERT INTO Teste (ID, Vacina, Pais, Quantidade, DataInicio, DataFim, Local)
-	VALUES (DEFAULT, 3, 5, 7000, current_timestamp, current_timestamp, 'Estado do Rio de Janeiro');
+INSERT INTO Teste (ID, Vacina, Quantidade, DataInicio, DataFim, Local)
+	VALUES (DEFAULT, 3, 7000, current_timestamp, current_timestamp, 'Estado do Rio de Janeiro');
 
-INSERT INTO Teste (ID, Vacina, Pais, Quantidade, DataInicio, DataFim, Local)
-	VALUES (DEFAULT, 1, 3, 2000, current_timestamp, current_timestamp, 'Ile de France');
+INSERT INTO Teste (ID, Vacina, Quantidade, DataInicio, DataFim, Local)
+	VALUES (DEFAULT, 1, 2000, current_timestamp, current_timestamp, 'Ile de France');
+
+
+-- RelTestPai
+
+INSERT INTO RelTestPai (Teste, Pais)
+	VALUES (1, 1);
+
+INSERT INTO RelTestPai (Teste, Pais)
+	VALUES (2, 1);
+
+INSERT INTO RelTestPai (Teste, Pais)
+	VALUES (2, 2);
+
+INSERT INTO RelTestPai (Teste, Pais)
+	VALUES (3, 3);
+
+INSERT INTO RelTestPai (Teste, Pais)
+	VALUES (4, 5);
 
 -- Acompanhamento
 
+INSERT INTO Acompanhamento (ID, Teste, Quantidade, Status, Biotipo, Local)
+	VALUES (DEFAULT, 1, 50, 'Ocorrendo', 'Profissionais de Saude', 'Hospital Morto Vivo');
 
-INSERT INTO Acompanhamento (ID, Teste, Quantidade, Status, Observacoes, Biotipo, Local)
-	VALUES (DEFAULT, 1, 50, 'Ocorrendo', 'Parece que funciona', 'Profissionais de Saude', 'Hospital Morto Vivo');
+INSERT INTO Acompanhamento (ID, Teste, Quantidade, Status, Biotipo, Local)
+	VALUES (DEFAULT, 1, 50, 'Ocorrendo', 'Idosos', 'Hospital Morto Vivo');
 
-INSERT INTO Acompanhamento (ID, Teste, Quantidade, Status, Observacoes, Biotipo, Local)
-	VALUES (DEFAULT, 1, 50, 'Ocorrendo', 'Morreram todos', 'Idosos', 'Hospital Morto Vivo');
+INSERT INTO Acompanhamento (ID, Teste, Quantidade, Status, Biotipo, Local)
+	VALUES (DEFAULT, 1, 50, 'Ocorrendo', 'Adultos', 'Hospital Morto Vivo');
 
-INSERT INTO Acompanhamento (ID, Teste, Quantidade, Status, Observacoes, Biotipo, Local)
-	VALUES (DEFAULT, 1, 50, 'Ocorrendo', 'Se recusaram a ser vacinados', 'Adultos', 'Hospital Morto Vivo');
+INSERT INTO Acompanhamento (ID, Teste, Quantidade, Status, Biotipo, Local)
+	VALUES (DEFAULT, 2, 70, 'Ocorrendo', 'Outro', 'Empresa 1');
 
-INSERT INTO Acompanhamento (ID, Teste, Quantidade, Status, Observacoes, Biotipo, Local)
-	VALUES (DEFAULT, 2, 70, 'Ocorrendo', '50% abandonou programa', 'Outro', 'Empresa 1');
-
-INSERT INTO Acompanhamento (ID, Teste, Quantidade, Status, Observacoes, Biotipo, Local)
-	VALUES (DEFAULT, 2, 70, 'Ocorrendo', 'Sem recursos para continuar', 'Adultos', 'Empresa 1');
+INSERT INTO Acompanhamento (ID, Teste, Quantidade, Status, Biotipo, Local)
+	VALUES (DEFAULT, 2, 70, 'Ocorrendo', 'Adultos', 'Empresa 1');
 
 
--- pg_dump -U joseluizjunior -W -F p projeto_bd1 > ~/Documents/Backup_1
+-- RelTestPai
+
+INSERT INTO RelVacTestAcom (Vacina, Teste, Acompanhamento)
+	VALUES (1, 1, 2);
+
+INSERT INTO RelVacTestAcom (Vacina, Teste, Acompanhamento)
+	VALUES (1, 2, 1);
+
+INSERT INTO RelVacTestAcom (Vacina, Teste, Acompanhamento)
+	VALUES (2, 3, 4);
+
+INSERT INTO RelVacTestAcom (Vacina, Teste, Acompanhamento)
+	VALUES (3, 4, 5);
+
+INSERT INTO RelVacTestAcom (Vacina, Teste, Acompanhamento)
+	VALUES (4, 4, 3);
+
+-- pg_dump -U joseluizjunior -W -F p projeto_bd1 > ~/Documents/Backup_2
+
+
+-- Consultas usando mais de duas tabelas no inner join
+
+-- Lista todas as vacinas mostrando a instrituicao, pais, vacina, fase e principio ativo ordenando pelo nome da vacina (ja que pode ter mais de uma instituicao nela)
+
+SELECT inst.nome AS "Instituicao", pais.nome AS "Pais", vac.nome AS "Vacina", vac.fase AS "Fase", vac.principio AS "Principio"
+FROM instituicao AS inst
+	INNER JOIN pais AS pais
+		ON inst.pais = pais.id
+	INNER JOIN RelInstVac AS rel
+		ON rel.instituicao = inst.id
+	INNER JOIN vacina AS vac
+		ON rel.vacina = vac.id
+	ORDER BY vac.nome ASC;
+
+-- Conta a quantidade de vacinas numa fase x em cada continente e ordena e agrupa por continente
+SELECT pais.continente AS "Continente",
+	COUNT(CASE WHEN vac.fase = '1' then 1 ELSE NULL END) as "Qtd Fase 1",
+	COUNT(CASE WHEN vac.fase = '2' then 1 ELSE NULL END) as "Qtd Fase 2",
+	COUNT(CASE WHEN vac.fase = '3' then 1 ELSE NULL END) as "Qtd Fase 3",
+	COUNT(vac.fase) as "Qtd Total"
+FROM instituicao AS inst
+	INNER JOIN pais AS pais
+		ON inst.pais = pais.id
+	INNER JOIN RelInstVac AS rel
+		ON rel.instituicao = inst.id
+	INNER JOIN vacina AS vac
+		ON rel.vacina = vac.id
+	GROUP BY pais.continente
+	ORDER BY pais.continente ASC;
+
+
+-- Consultas usando like
+-- Lista o nome das instituicoes e local dela caso tenha a string de no meio do nome do governo e ordena pelo nome
+
+SELECT inst.nome AS "Instituicao",
+	inst.local AS "Local"
+	FROM instituicao as inst
+WHERE inst.governo LIKE '%de%'
+ORDER BY inst.nome ASC;
+
+
+-- Lista os lugares dos testes onde o nome do lugar tem pelo menos 11 caracteres e ordena pela quantidade
+
+SELECT vac.nome AS "Vacina",
+	teste.quantidade AS "Quantidade",
+	teste.local AS "Local"
+	FROM teste
+	INNER JOIN vacina AS vac
+		ON teste.vacina = vac.id
+WHERE teste.local LIKE '___________%'
+ORDER BY teste.quantidade ASC;
+
+
+-- Funcao de agregacao usando having
+-- Maior quantidade de distribuicao em cada se a menor da cidade for maior que 900
+SELECT dist.local AS "Local",
+	max(dist.quantidade) AS "Maior quantidade"
+	FROM distribuicao AS dist
+	GROUP BY dist.local
+	HAVING min(dist.quantidade) > 900;
+
+-- Mostra a soma de todas as distribuicoes de cada cidade se a soma for maior que 1900 
+-- e ordenando da maior quantidade para a maior
+SELECT dist.local AS "Local",
+	SUM(dist.quantidade) AS "Total"
+	FROM distribuicao AS dist
+	GROUP BY dist.local
+	HAVING SUM(dist.quantidade) > 1900
+	ORDER BY SUM(dist.quantidade) DESC;
+
+
+-- Junção externa
+
+
+-- Mostra as informacoes do teste e faz a juncao a direita com a tabela vacina, logo teremos as vacinas sem teste ao final
+-- por conta da ordenacao ascendente da data, já que nao existe teste sem vacina, porem existe vacina sem teste
+-- entao nao faz nem sentido fazer uma juncao externa full, o resultado seria o mesmo
+SELECT
+	teste.datainicio AS "Data de Inicio",
+	teste.datafim AS "Data de Fim",
+	teste.local AS "Local",
+	teste.quantidade AS "Quantidade",
+	vac.nome AS "Vacina"
+FROM teste
+RIGHT OUTER JOIN vacina as vac ON (vac.id=teste.vacina)
+ORDER BY teste.datafim ASC;
+
+
+-- Ao fazer o left outer join teremos o mesmo resultado que um join normal já que nao existe teste sem vacina
+SELECT
+	teste.datainicio AS "Data de Inicio",
+	teste.datafim AS "Data de Fim",
+	teste.local AS "Local",
+	teste.quantidade AS "Quantidade",
+	vac.nome AS "Vacina"
+FROM teste
+LEFT OUTER JOIN vacina as vac ON (vac.id=teste.vacina)
+ORDER BY teste.datafim ASC;
+
+
+-- Divisao
+
+-- Retorna a porcentagem das vacinas em cada fase na totalidade
+SELECT	CAST(COUNT(CASE WHEN vac.fase = '1' then 1 ELSE NULL END) AS FLOAT) / COUNT(vac.fase) AS "Porcentagem na Fase 1",
+	CAST(COUNT(CASE WHEN vac.fase = '2' then 1 ELSE NULL END) AS FLOAT) / COUNT(vac.fase) AS "Porcentagem na Fase 1",
+	CAST(COUNT(CASE WHEN vac.fase = '3' then 1 ELSE NULL END) AS FLOAT) / COUNT(vac.fase) AS "Porcentagem na Fase 1",
+	COUNT(vac.fase) AS "TOTAL"
+FROM vacina AS vac
+
+-- Retorna a porcentagem das vacinas em cada fase para cada pais
+SELECT pais.nome AS "Pais",
+	COUNT(CASE WHEN vac.fase = '1' then 1 ELSE NULL END) AS "Qtd Fase 1",
+	CAST(COUNT(CASE WHEN vac.fase = '1' then 1 ELSE NULL END) AS FLOAT) / COUNT(vac.fase) AS "% Fase 1",
+	COUNT(CASE WHEN vac.fase = '2' then 1 ELSE NULL END) AS "Qtd Fase 2",
+	CAST(COUNT(CASE WHEN vac.fase = '2' then 1 ELSE NULL END) AS FLOAT) / COUNT(vac.fase) AS "% Fase 2",
+	COUNT(CASE WHEN vac.fase = '3' then 1 ELSE NULL END) AS "Qtd Fase 3",
+	CAST(COUNT(CASE WHEN vac.fase = '3' then 1 ELSE NULL END) AS FLOAT) / COUNT(vac.fase) AS "% Fase 3",
+	COUNT(vac.fase) AS "TOTAL"
+FROM instituicao AS inst
+	INNER JOIN pais AS pais
+		ON inst.pais = pais.id
+	INNER JOIN RelInstVac AS rel
+		ON rel.instituicao = inst.id
+	INNER JOIN vacina AS vac
+		ON rel.vacina = vac.id
+	GROUP BY pais.nome
+	ORDER BY pais.nome ASC;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
